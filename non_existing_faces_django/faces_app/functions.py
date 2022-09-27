08657af624970e18799b419bff41eb1abb175afe
AdianKozlica <kozlicaadian@gmail.com>
2022-09-27 20:30:24 +0200

    Setup django

---
+++ non_existing_faces_django/faces_app/functions.py
@@ -0,0 +1,6 @@
+import cv2
+import random_face
+import numpy as np
+
+from . import models
+
--- non_existing_faces_django/non_existing_faces_django/settings.py
+++ non_existing_faces_django/non_existing_faces_django/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "faces_app"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'non_existing_faces_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'var/static_root/'
-STATICFILES_DIRS = ['static']
+STATIC_ROOT = BASE_DIR / "var" / "static_root"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
