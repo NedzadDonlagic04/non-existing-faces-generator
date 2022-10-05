1769476679a84adbb82b7681131b2bc84b8b88c4
AdianKozlica <105174725+AdianKozlica@users.noreply.github.com>
2022-10-05 15:24:31 +0200

    Update urls.py

--- non_existing_faces_django/faces_app/urls.py
+++ non_existing_faces_django/faces_app/urls.py
@@ -1,6 +1,8 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path("",views.index)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
