e9a5b9d69570c4ad9871da1be68ac830ad63dd88
AdianKozlica <105174725+AdianKozlica@users.noreply.github.com>
2022-10-05 16:05:59 +0200

    Update settings.py

--- non_existing_faces_django/non_existing_faces_django/settings.py
+++ non_existing_faces_django/non_existing_faces_django/settings.py
@@ -69,37 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'non_existing_faces_django.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
