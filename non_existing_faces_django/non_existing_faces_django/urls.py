6bb74fcc6fc987f7d64b5716ab4290b8d0a67c67
AdianKozlica <105174725+AdianKozlica@users.noreply.github.com>
2022-10-09 14:25:34 +0200

    Update urls.py

--- non_existing_faces_django/non_existing_faces_django/urls.py
+++ non_existing_faces_django/non_existing_faces_django/urls.py
@@ -14,9 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path,include
-from django.contrib import admin
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path("",include("faces_app.urls"))
 ]
